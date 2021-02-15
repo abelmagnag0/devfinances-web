dc4d3923a854c6ca0f402e34a6a1127faa5989a7
Abel Magnago <abel.magnago@gmail.com>
2021-02-15 18:57:09 -0300

    Add category feature and modal changes

--- scripts/home.js
+++ scripts/home.js
@@ -1,10 +1,10 @@
 const Storage = {
-  get(){
+  get() {
     return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
   },
-  set(transactions){
+  set(transactions) {
     localStorage.setItem("dev.finances:transactions",
-    JSON.stringify(Transaction.all))
+      JSON.stringify(Transaction.all))
   }
 }
 
@@ -14,9 +14,16 @@ const Transaction = {
     Transaction.all.push(transaction)
     App.reload()
   },
+  areYouSure(index){
+     const message = `<button onclick="Modal.handleAddedModal()"><img src="./assets/close.svg" alt="Fechar"><h2 class="sr-only">Fechar</h2></button><h2 style="text-align: left;">O(a) senhor(a) tem certeza que gostaria de excluir a transação?</h2><a href="#" class="button cancel" onclick="Transaction.remove(${index})">Sim, desejo excluir a transação permanentemente.</a>`
+    
+    const modal = Modal.createModal(message)
+    
+  },
   remove(index) {
-    Transaction.all.splice(index, 1)
-    App.reload()
+      Modal.handleAddedModal()
+      Transaction.all.splice(index, 1)
+      App.reload()
   },
   incomes() {
     let income = 0
@@ -63,15 +70,16 @@ const DOM = {
     const html = `<td class="description">${transaction.description}</td>
     <td class="${cssClass}">${Utils.formatCurrency(transaction.amount)}</td>
     <td class="date">${transaction.date}</td>
+    <td class="category">${transaction.category}</td>
     <td class="remove">
-    <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover Transação"
+    <img onclick="Transaction.areYouSure(${index})" src="./assets/minus.svg" alt="Remover Transação"
     </td>
       `
     return html
   },
   addEachTransaction() {
     Transaction.all.forEach((transaction, index) => {
-      this.addTransaction(transaction,index)
+      this.addTransaction(transaction, index)
     })
   },
   updateBalance() {
@@ -101,6 +109,26 @@ const Utils = {
     const [year, month, day] = splittedDate
     return `${day}/${month}/${year}`
   },
+  formatCategory(category) {
+    switch (category) {
+      case "0":
+        category = "Renda"
+        break
+      case "1":
+        category = "Lazer"
+        break
+      case "2":
+        category = "Despesas da casa"
+        break
+      case "3":
+        category = "Despesas de automóveis"
+        break
+      case "4":
+        category = "Outros"
+        break
+    }
+    return category
+  },
   formatCurrency(value) {
     const signal = Number(value) < 0 ? "-" : ""
 
@@ -113,6 +141,14 @@ const Utils = {
       currency: "BRL"
     })
     return signal + value
+  },
+  buttonValue(button){
+    if(button.value == 0){
+      return "Não"
+    }
+    if(button.value == 1){
+      return "Sim"
+    }
   }
 }
 
@@ -120,36 +156,43 @@ const Form = {
   description: document.querySelector("input#description"),
   amount: document.querySelector("input#amount"),
   date: document.querySelector("input#date"),
+  category: document.querySelector("select#category"),
   getValues() {
     return {
       description: Form.description.value,
       amount: Form.amount.value,
-      date: Form.date.value
+      date: Form.date.value,
+      category: Form.category.value
     }
   },
   validateFields() {
-    const { description, amount, date } = Form.getValues()
+    const { description, amount, date, category } = Form.getValues()
 
-    if (description.trim() == "" || amount.trim() == "" || date.trim() == "") {
+    if (description.trim() == "" || amount.trim() == "" || date.trim() == "" || category.trim() == "") {
       throw new Error("Por favor, preencha todos os campos")
     }
 
   },
   formatValues() {
-    let { description, amount, date } = Form.getValues()
+    let { description, amount, date, category } = Form.getValues()
 
     amount = Utils.formatAmount(amount)
     date = Utils.formatDate(date)
+    category = Utils.formatCategory(category)
 
     return {
       description,
       amount,
-      date
+      date,
+      category
     }
+  },
+  transactionAdded() {
+    const message = `<button onclick="Modal.handleAddedModal()"><img src="./assets/close.svg" alt="Fechar"><h2 class="sr-only">Fechar</h2></button><h2>Transação adicionada com sucesso!</h2>`
+    Modal.createModal(message)
   },
   saveTransaction(transaction) {
     Transaction.add(transaction)
-    alert("Transação adicionada!")
   },
   clearForm() {
     Form.description.value = ""
@@ -165,6 +208,7 @@ const Form = {
       this.saveTransaction(transaction)
       this.clearForm()
       Modal.handleModal()
+      this.transactionAdded()
     } catch (error) {
       alert(error.message)
     }
--- scripts/modal.js
+++ scripts/modal.js
@@ -3,6 +3,31 @@ const Modal = {
     const modal = document.querySelector(".modal-overlay")
     modal.classList.toggle("active")
   },
+  handleAddedModal() {
+    const modal = document.querySelectorAll(".modal-overlay")[1]
+    modal.classList.toggle("active")
+    
+    const body = document.querySelector("body")
+    body.removeChild(modal)
+  },
+  createModal(message){
+    const modalOverlay = document.createElement("div")
+    const modal = document.createElement("div")
+    modalOverlay.classList.add("modal-overlay")
+    modalOverlay.classList.add("active")
+    modal.classList.add("modal")
+    modal.classList.add("highest")
+
+    const div = document.createElement("div")
+    div.classList.add("modal-button")
+    div.innerHTML = `${message}`
+
+    modal.appendChild(div)
+    modalOverlay.appendChild(modal)
+
+    const body = document.querySelector("body")
+    body.appendChild(modalOverlay)
+  },
   start() {
     document.querySelector(".button.new")
       .addEventListener("click", this.handleModal)
